cf9c476cbd2eec4e3c5c42100ff7b84319a5eb6a
Jonathan T Cantu <Jonathant.cantu@gmail.com>
2025-10-08 15:49:28 -0500

    localHost is no longer supported on  spotify so I had to update the config folder to an ip address instead

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '127.0.0.1', // bind to loopback IP instead of localhost
+    port: 5173,        // keep the same port
+  },
 })
